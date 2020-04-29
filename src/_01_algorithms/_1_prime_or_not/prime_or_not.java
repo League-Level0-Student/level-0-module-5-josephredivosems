42a8fc577adce160b245143e2c18c9d2bf7fffa6
root <root@61791b163ff6>
2020-04-29 23:26:44 +0000

    hello

--- src/_01_algorithms/_1_prime_or_not/prime_or_not.java
+++ src/_01_algorithms/_1_prime_or_not/prime_or_not.java
@@ -6,16 +6,21 @@ public class prime_or_not {
 public static void main(String[] args) {
 	String convert = JOptionPane.showInputDialog("What number would you like to know if it is prime or not?");
 int prime = Integer.parseInt(convert);	
-if(prime % 2 == 0) {
-	if(prime == 2) {
-		JOptionPane.showMessageDialog(null, "Your number is prime");
-	}
-	else {
-		JOptionPane.showMessageDialog(null, "Your number isn't prime");
-	}
+boolean isPrime = true;
+for (int i = 2; i < prime; i++) {
+	if(prime % i == 0) {
+		isPrime = false;
+		
+		
+		}
+	
 }
-
-
+if (isPrime == false) {
+	JOptionPane.showMessageDialog(null, "Your number isn't prime");
+}
+else {
+	JOptionPane.showMessageDialog(null, "Your number is prime");
+}
 
 
 
---
+++ src/_01_algorithms/_2_fibonacci/fibonacci.java
@@ -0,0 +1,15 @@
+package _01_algorithms._2_fibonacci;
+
+public class fibonacci {
+public static void main(String[] args) {
+	int number1 = 0;
+	int number2 = 1;
+	int sum = 1;
+	for (int i = 0; i < 10; i++) {
+		sum = number1 + number2;
+		System.out.println(sum);
+		number1 = number2;
+		number2 = sum;
+	}
+}
+}
